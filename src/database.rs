e145f75354270b1be3485a08748a26085ca7d8a3
Gryffon <owenbellish@gmail.com>
2021-10-28 18:54:06 +0000

    more changes, needs fixing

--- src/database.rs
+++ src/database.rs
@@ -1,12 +1,15 @@
 #![allow(dead_code)]
+use serde::{Deserialize, Serialize};
+
 use crate::backend::Backend;
-use std::sync::Arc;
+use std::{any::TypeId, sync::Arc};
 
 /// todo
 #[derive(Debug)]
 pub struct Database<B: Backend> {
     table_name: String,
     backend: Arc<B>,
+    type_id: Option<TypeId>,
 }
 
 impl<B: Backend> Database<B> {
@@ -15,7 +18,48 @@ impl<B: Backend> Database<B> {
         Self {
             table_name,
             backend,
+            type_id: None,
         }
+    }
+
+    /// todo
+    /// 
+    /// # Panics
+    /// 
+    /// todo
+    pub fn kind<V>(&self) 
+    where V: Serialize + for<'de> Deserialize<'de> + 'static {
+        let type_id = TypeId::of::<V>();
+
+        if self.type_id.is_some() && self.type_id != Some(type_id) {
+            panic!("expected type_id to match")
+        }
+
+        if self.type_id.is_none() {
+            Self::set_type_id(&mut self, type_id);
+        }
+    }
+
+    fn set_type_id(&mut self, type_id: TypeId) {
+        self.type_id = Some(type_id);
+    }
+
+    /// todo
+    /// 
+    /// # Panics
+    /// 
+    /// todo
+    /// 
+    /// # Errors
+    /// 
+    /// todo
+    pub async fn get<'a, D>(&self, id: &str) -> Result<Option<D>, B::Error>
+    where
+        D: for<'de> Deserialize<'de> + 'static,
+    {
+        let type_id = TypeId::of::<D>();
+
+        todo!()
     }
 }
 
@@ -24,6 +68,7 @@ impl<B: Backend> Clone for Database<B> {
         Self {
             backend: self.backend.clone(),
             table_name: self.table_name.clone(),
+            type_id: self.type_id,
         }
     }
 }
