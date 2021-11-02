11cc211fb2b0b01981c904572500906791bdf793
Gryffon <owenbellish@gmail.com>
2021-11-02 11:35:18 +0000

    add key trait and docs

--- src/entry.rs
+++ src/entry.rs
@@ -1,11 +1,23 @@
 use serde::{de::DeserializeOwned, Serialize};
 use std::fmt::Debug;
 
-pub trait Key {}
+/// The key trait to be implemented on [`Settings`] to allow an easy way to get keys.
+pub trait Key {
+    /// The method to transform a [`Key`] into a value.
+    fn to_key(&self) -> String;
+}
+
+impl<T> Key for T where T: Settings + ToString {
+    fn to_key(&self) -> String {
+        self.to_string()
+    }
+}
 
 /// A marker trait for use within the [`Database`].
 ///
 /// This signifies that the type can be stored within a [`Database`].
+/// 
+/// [`Database`]: crate::Database
 pub trait Settings: Serialize + DeserializeOwned + Debug + Send + Sync {}
 
 impl<T> Settings for T where T: Serialize + DeserializeOwned + Debug + Send + Sync {}
