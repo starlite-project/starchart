e1cf525831a60a1cd6eb99ef8ca28eb28cc92011
PyroTechniac <PyroTechniac@users.noreply.github.com>
2021-11-16 18:30:30 +0000

    deploy: 45f3a6dc561805b90f5a7869b7ab470e0a9aa814

--- starchart/atomics/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"enum":[["GuardKind","The kind of lock an [`AtomicGuard`] is holding."]],"struct":[["AtomicGuard","The mechanism used to allow multiple readers and only one writer to access a shared database."],["ExclusiveGuard","An exclusive guard for allowing only one access to a resource."],["SharedGuard","A shared guard for allowing multiple accesses to a resource."]]});
