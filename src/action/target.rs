d6f508f574b0d54ae9b9114b97698177b8ee7013
Gryffon <owenbellish@gmail.com>
2021-11-22 17:24:52 +0000

    target tests

--- src/action/target.rs
+++ src/action/target.rs
@@ -27,3 +27,37 @@ impl Display for OperationTarget {
 		}
 	}
 }
+
+#[cfg(test)]
+mod tests {
+	use std::fmt::{Debug, Display};
+
+	use serde::{Deserialize, Serialize};
+	use static_assertions::assert_impl_all;
+
+	use super::OperationTarget;
+
+	assert_impl_all!(
+		OperationTarget: Clone,
+		Copy,
+		Debug,
+		Default,
+		Deserialize<'static>,
+		Display,
+		PartialEq,
+		Send,
+		Serialize,
+		Sync
+	);
+
+	#[test]
+	fn default() {
+		assert_eq!(OperationTarget::default(), OperationTarget::Entry);
+	}
+
+	#[test]
+	fn display() {
+		assert_eq!(OperationTarget::Entry.to_string(), "Entry");
+		assert_eq!(OperationTarget::Table.to_string(), "Table");
+	}
+}
