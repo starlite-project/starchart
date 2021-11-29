234a27351ddb10934847b3fc516006561c32a7f1
Gryffon <owenbellish@gmail.com>
2021-11-29 18:05:42 +0000

    first example

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 A database wrapper that allowes for multiple backends
 
+Starchart is a powerful, yet simple ORM, written to be safe, atomic, and fast.
+
 # MSRV
 
 The Minimum supported Rust version is 1.46
---
+++ examples/basic.rs
@@ -0,0 +1,75 @@
+use std::sync::atomic::{AtomicU64, Ordering};
+
+use serde::{Deserialize, Serialize};
+use starchart::{
+	action::{CreateEntryAction, CreateTableAction, ReadEntryAction},
+	backend::CacheBackend,
+	Action, ChartResult, IndexEntry, Starchart,
+};
+
+static IDS: AtomicU64 = AtomicU64::new(1);
+
+#[derive(
+	Debug, Default, Clone, PartialEq, Eq, PartialOrd, Ord, Serialize, Deserialize, IndexEntry,
+)]
+struct Settings {
+	id: u64,
+	name: String,
+	age: u8,
+}
+
+impl Settings {
+	pub fn new(name: String, age: u8) -> Self {
+		let id = IDS.fetch_add(1, Ordering::SeqCst);
+
+		Self { id, name, age }
+	}
+}
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> ChartResult<(), CacheBackend> {
+	// Create an initialize the database.
+	let chart = Starchart::new(CacheBackend::default()).await?;
+
+	// Create and run an action to create the "foo" table with the Settings struct.
+	let mut create_table_action: CreateTableAction<Settings> = Action::new();
+
+	create_table_action.set_table("foo");
+
+	chart.run(create_table_action).await??;
+
+	// Insert some entries into the table.
+	for (age, name) in vec![
+		(21, "John Doe".to_owned()),
+		(42, "Ferris".to_owned()),
+		(73, "The Queen".to_owned()),
+	] {
+		let mut action: CreateEntryAction<Settings> = Action::new();
+		action.set_table("foo").set_entry(&Settings::new(name, age));
+		chart.run(action).await??;
+	}
+
+	// Get a single entry.
+
+	let the_queen: Settings = {
+		// Action type helpers are named after their CRUD counterparts; Create, Read, Update, and Delete.
+		let mut action: ReadEntryAction<Settings> = Action::new();
+		action.set_key(&3_u64).set_table("foo");
+
+		chart
+			.run(action)
+			.await??
+			.expect("failed to find the queen!")
+	};
+
+	assert_eq!(
+		the_queen,
+		Settings {
+			id: 3,
+			name: "The Queen".to_owned(),
+			age: 73
+		}
+	);
+
+	Ok(())
+}
