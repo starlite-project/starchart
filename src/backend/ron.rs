5897c44cc8ba092707ecbde0f5a2371567d971e3
Gryffon <owenbellish@gmail.com>
2021-11-30 15:44:12 +0000

    add ron tests

--- src/backend/ron.rs
+++ src/backend/ron.rs
@@ -55,3 +55,174 @@ impl FsBackend for RonBackend {
 		self.0.clone()
 	}
 }
+
+#[cfg(all(test, feature = "ron"))]
+mod tests {
+	use std::{fmt::Debug, fs, path::PathBuf};
+
+	use static_assertions::assert_impl_all;
+
+	use crate::{
+		backend::{Backend, FsError, RonBackend},
+		util::testing::FsCleanup as Cleanup,
+	};
+
+	assert_impl_all!(RonBackend: Backend, Clone, Debug, Default, Send, Sync);
+
+	#[test]
+	fn new() -> Result<(), FsError> {
+		let path = Cleanup::new("new", "ron", true)?;
+		let _blank = Cleanup::new("", "ron", true)?;
+		let backend = RonBackend::new(&path)?;
+
+		assert_eq!(backend.0, PathBuf::from(&path));
+
+		let file_path = Cleanup::new("file.txt", "ron", false)?;
+
+		fs::write(&file_path, "Hello, world!")?;
+
+		assert!(RonBackend::new(&file_path).is_err());
+
+		fs::remove_file(file_path)?;
+
+		Ok(())
+	}
+
+	#[tokio::test]
+	#[cfg_attr(miri, ignore)]
+	async fn init() -> Result<(), FsError> {
+		let path = Cleanup::new("init", "ron", false)?;
+		let backend = RonBackend::new(&path)?;
+
+		backend.init().await?;
+
+		assert!(fs::read_dir(&path).is_ok());
+
+		backend.init().await?;
+
+		Ok(())
+	}
+
+	#[tokio::test]
+	#[cfg_attr(miri, ignore)]
+	async fn has_and_create_table() -> Result<(), FsError> {
+		let path = Cleanup::new("has_and_create_table", "ron", true)?;
+		let backend = RonBackend::new(&path)?;
+
+		backend.init().await?;
+
+		assert!(!backend.has_table("table").await?);
+
+		backend.create_table("table").await?;
+
+		assert!(backend.has_table("table").await?);
+
+		Ok(())
+	}
+
+	#[tokio::test]
+	#[cfg_attr(miri, ignore)]
+	async fn get_keys() -> Result<(), FsError> {
+		let path = Cleanup::new("get_keys", "ron", true)?;
+		let backend = RonBackend::new(&path)?;
+
+		backend.init().await?;
+
+		backend.create_table("table").await?;
+
+		backend.create("table", "id", &1).await?;
+		backend.create("table", "id2", &2).await?;
+
+		let mut keys: Vec<String> = backend.get_keys("table").await?;
+		let mut expected = vec!["id".to_owned(), "id2".to_owned()];
+
+		keys.sort();
+		expected.sort();
+
+		assert_eq!(keys, expected);
+
+		Ok(())
+	}
+
+	#[tokio::test]
+	#[cfg_attr(miri, ignore)]
+	async fn create_and_delete_table() -> Result<(), FsError> {
+		let path = Cleanup::new("create_and_delete_table", "ron", true)?;
+		let backend = RonBackend::new(&path)?;
+
+		backend.init().await?;
+
+		backend.create_table("table").await?;
+
+		assert!(backend.has_table("table").await?);
+
+		backend.delete_table("table").await?;
+
+		assert!(!backend.has_table("table").await?);
+
+		Ok(())
+	}
+
+	#[tokio::test]
+	#[cfg_attr(miri, ignore)]
+	async fn get_and_create() -> Result<(), FsError> {
+		let path = Cleanup::new("get_and_create", "ron", true)?;
+		let backend = RonBackend::new(&path)?;
+
+		backend.init().await?;
+
+		backend.create_table("table").await?;
+
+		backend.create("table", "id", &1_u8).await?;
+
+		assert_eq!(backend.get::<u8>("table", "id").await?, Some(1));
+
+		assert_eq!(backend.get::<u8>("table", "id2").await?, None);
+
+		assert!(backend.create("table", "id", &2_u8).await.is_err());
+
+		Ok(())
+	}
+
+	#[tokio::test]
+	#[cfg_attr(miri, ignore)]
+	async fn update_and_replace() -> Result<(), FsError> {
+		let path = Cleanup::new("update_and_replace", "ron", true)?;
+		let backend = RonBackend::new(&path)?;
+
+		backend.init().await?;
+
+		backend.create_table("table").await?;
+
+		backend.create("table", "id", &1_u8).await?;
+
+		backend.update("table", "id", &2_u8).await?;
+
+		assert_eq!(backend.get::<u8>("table", "id").await?, Some(2));
+
+		backend.replace("table", "id", &3_u8).await?;
+
+		assert_eq!(backend.get::<u8>("table", "id").await?, Some(3));
+
+		Ok(())
+	}
+
+	#[tokio::test]
+	#[cfg_attr(miri, ignore)]
+	async fn delete() -> Result<(), FsError> {
+		let path = Cleanup::new("delete", "ron", true)?;
+		let backend = RonBackend::new(&path)?;
+
+		backend.init().await?;
+
+		backend.create_table("table").await?;
+
+		backend.create("table", "id", &1_u8).await?;
+
+		backend.delete("table", "id").await?;
+
+		assert_eq!(backend.get::<u8>("table", "id").await?, None);
+
+		Ok(())
+	}
+}
