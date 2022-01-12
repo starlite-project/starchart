636a817cee9b5fcd4900816e14a1c94b4169a7af
Gryffon <owenbellish@gmail.com>
2022-01-12 18:15:02 +0000

    first nightly feature

--- build.rs
+++ build.rs
@@ -1,8 +1,17 @@
 use rustc_version::{version_meta, Channel, Error};
+use std::env;
 
 fn main() -> Result<(), Error> {
-	if let Channel::Nightly = version_meta()?.channel {
+	let version_data = version_meta()?;
+	if let Channel::Nightly = version_data.channel {
 		println!("cargo:rustc-cfg=docsrs");
+		if env::var("CARGO_FEATURE_NIGHTLY").ok().is_some() {
+			println!("cargo:rustc-cfg=nighttime"); // for fun
+		}
+
+		if version_data.semver.minor >= 58 {
+			println!("cargo:rustc-cfg=unwrap_unchecked")
+		}
 	}
 
 	Ok(())
