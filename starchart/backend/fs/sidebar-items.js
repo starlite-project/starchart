7ee88f8f9bb368be2aeece67eb8e09b517557586
PyroTechniac <PyroTechniac@users.noreply.github.com>
2022-01-12 17:07:42 +0000

    deploy: ebfb1139f76de4c7daad36577a3fbeed785b182c

--- starchart/backend/fs/sidebar-items.js
+++ starchart/backend/fs/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["FsError","An error occurred from an [`FsBackend`]."]],"trait":[["FsBackend","The trait for all File System based backends to implement"]]});
+initSidebarItems({"enum":[["FsErrorType","The type of [`FsError`] that occurred."]],"struct":[["FsError","An error occurred from an [`FsBackend`]."]],"trait":[["FsBackend","The trait for all File System based backends to implement"]]});
