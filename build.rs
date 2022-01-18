aa44930e1d1326ebc2d27199b5a23bd2cec544de
Gryffon <owenbellish@gmail.com>
2022-01-18 18:32:42 +0000

    remove commented out code

--- build.rs
+++ build.rs
@@ -5,13 +5,6 @@ fn main() -> Result<(), Error> {
 	let minor = version_data.semver.minor;
 	if let Channel::Nightly = version_data.channel {
 		println!("cargo:rustc-cfg=docsrs");
-		// if env::var("CARGO_FEATURE_NIGHTLY").ok().is_some() {
-		// 	println!("cargo:rustc-cfg=nighttime"); // for fun
-		// }
-
-		// if version_data.semver.minor >= 58 {
-		// 	println!("cargo:rustc-cfg=unwrap_unchecked")
-		// }
 	}
 
 	if minor < 58 {
