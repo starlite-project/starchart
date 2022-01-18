9e7dcec2cf9a9aa6468c08483b5eb76ecfa5895c
PyroTechniac <PyroTechniac@users.noreply.github.com>
2022-01-18 19:51:55 +0000

    deploy: aef15bb3d9951dbdce2f90c7ed016175b524b997

--- source-files.js
+++ source-files.js
@@ -1,4 +1,4 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["starchart"] = {"name":"","dirs":[{"name":"action","files":["error.rs","impl.rs","kind.rs","mod.rs","result.rs","target.rs"]},{"name":"backend","files":["bincode.rs","fs.rs","future.rs","json.rs","memory.rs","mod.rs","toml.rs","yaml.rs"]}],"files":["atomics.rs","entry.rs","error.rs","lib.rs","starchart.rs","util.rs"]};
+sourcesIndex["starchart"] = {"name":"","dirs":[{"name":"action","files":["error.rs","impl.rs","kind.rs","mod.rs","result.rs","target.rs"]},{"name":"backend","files":["bincode.rs","fs.rs","futures.rs","json.rs","memory.rs","mod.rs","toml.rs","yaml.rs"]}],"files":["atomics.rs","entry.rs","error.rs","lib.rs","starchart.rs","util.rs"]};
 sourcesIndex["starchart_derive"] = {"name":"","files":["lib.rs"]};
 createSourceSidebar();
