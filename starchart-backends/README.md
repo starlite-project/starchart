2cbe7b870fa1da9c4e427e214436b7586dad7a57
Gryffon <owenbellish@gmail.com>
2022-02-12 18:37:29 +0000

    fix symlinks

--- starchart-backends/README.md
+++ starchart-backends/README.md
@@ -1,1 +1,9 @@
-S:/GitHub/starchart/README.md
+# Starchart
+
+A database wrapper that allowes for multiple backends
+
+Starchart is a powerful, yet simple ORM, written to be safe, atomic, and fast.
+
+# MSRV
+
+The Minimum supported Rust version is 1.46
--- starchart-backends/build.rs
+++ starchart-backends/build.rs
@@ -1,1 +1,39 @@
-S:/GitHub/starchart/build.rs
+use std::fmt::{Display, Formatter, Result as FmtResult};
+
+use rustc_version::{version_meta, Channel, Error};
+
+// this is wholley unneeded but it makes things easier.
+#[derive(Debug, Clone, Copy)]
+enum CfgKeys {
+	Docsrs,
+	NoUnwrapUnchecked,
+	NoDebugNonExhaustive,
+}
+
+impl Display for CfgKeys {
+	fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+		match self {
+			Self::Docsrs => f.write_str("docsrs"),
+			Self::NoUnwrapUnchecked => f.write_str("no_unwrap_unchecked"),
+			Self::NoDebugNonExhaustive => f.write_str("no_debug_non_exhaustive"),
+		}
+	}
+}
+
+fn main() -> Result<(), Error> {
+	let version_data = version_meta()?;
+	let minor = version_data.semver.minor;
+	if let Channel::Nightly = version_data.channel {
+		println!("cargo:rustc-cfg={}", CfgKeys::Docsrs);
+	}
+
+	if minor < 58 {
+		println!("cargo:rustc-cfg={}", CfgKeys::NoUnwrapUnchecked);
+	}
+
+	if minor < 53 {
+		println!("cargo:rustc-cfg={}", CfgKeys::NoDebugNonExhaustive);
+	}
+
+	Ok(())
+}
